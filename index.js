6b8933607a823ebdf4f9f87161bb08d5e0cb9d6f
Developing-notes <101121374+Developing-notes@users.noreply.github.com>
2024-04-16 23:35:36 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+
+const client = new TwitterApi({
+    appKey: '***',
+    appSecret: '***',
+    accessToken: '****',
+    accessSecret: '***',
+    bearerToken: "***",
+});
+const rwClient = client.readWrite;
+const textTweet = async () => {
+    try {
+        let result = await rwClient.v2.tweet("hello guys")
+            console.log("success");
+            console.log("result: ", result);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+
+textTweet()
+
+const tweetId = '*****'
+
+const deleteTweet = async () => {
+    try {
+        let result = await client.v2.deleteTweet(tweetId);
+        console.log("result: ", result);
+        console.log(`Tweet ${tweetId} deleted successfully.`);
+    } catch (error) {
+        console.error(`Error deleting tweet ${tweetId}:`, error);
+    }
+}
+
+const tweetId = '*****'
+deleteTweet(tweetId)
+
+ReferenceLink :https://www.geeksforgeeks.org/tweet-using-node-js-and-twitter-api/
